064c97ed515fa5a26228cb417ad368915ff84c70
waldo237 <waldo23792@gmail.com>
2020-12-18 14:51:04 -0400

    practiced the linkedlist today

--- linkedLists/LinkedList2.js
+++ linkedLists/LinkedList2.js
@@ -16,17 +16,17 @@ class LinkedList {
         this.tail = null;
     }
 
-     equal(a, b) {
+    equal(a, b) {
         function compare(value1, value2) {
             if (a === b) return 0;
             return a < b ? -1 : 1;
         }
         return compare(a, b) === 0;
     }
-  
-    append(data){
+
+    append(data) {
         const node = new Node(data);
-        if(!this.head){
+        if (!this.head) {
             this.head = node;
             this.tail = node;
             return this;
@@ -36,31 +36,97 @@ class LinkedList {
         return this;
     }
 
-    delete(value) {
+    prepend(data) {
+        const node = new Node(data, this.head);
+        this.head = node;
+        if (!this.tail) {
+            this.tail = node;
+        }
+        return this;
+    }
+
+    delete(data) {
         if (!this.head) {
             return null;
         }
         let deletedNode = null;
-        while (this.head && this.equal(this.head.value, value)) {
+
+        /*if the data to be deleted is in the head, 
+        point the head to the next node
+        */
+        while (this.head && this.equal(this.head.value, data)) {
             deletedNode = this.head;
             this.head = this.head.next;
         }
 
-        let currentNode = this.head
+        /*if the data to be deleted is in the next node, 
+       point the next node to the one next to it.
+       */
+        let currentNode = this.head;
         while (currentNode && currentNode.next) {
-            if (this.equal(currentNode.next.value, value)) {
+            if (this.equal(currentNode.next.value, data)) {
                 deletedNode = currentNode.next;
-                currentNode = currentNode.next.next;
+                currentNode.next = currentNode.next.next;
             } else {
                 currentNode = currentNode.next;
             }
         }
 
-        if (this.equal(this.tail.value, value)) {
+        if (this.equal(this.tail.value, data)) {
             this.tail = currentNode;
         }
 
         return deletedNode;
+    }
+
+    deleteLastValue() {
+        const deletedTail = this.tail;
+
+        if (this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+
+            return deletedTail;
+        }
+
+        let currentNode = this.head;
+        while (currentNode.next) {
+            if (!currentNode.next.next) {
+                currentNode.next = null;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+        this.tail = currentNode;
+        return deletedTail;
+    }
+
+    deleteFirstValue(){
+        if(!this.head){
+            return null;
+        }
+        const deletedHead = this.head;
+        if(this.head.next){
+            this.head = this.head.next;
+        }else{
+            this.head = null;
+            this.tail = null;
+        }
+        return deletedHead;
+    }
+
+    find(value = undefined) {
+        if (!this.head) {
+            return null;
+        }
+        let currentNode = this.head;
+        while (currentNode) {
+            if (value !== undefined && this.equal(currentNode.value, value)) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
     }
 
 }
@@ -69,8 +135,13 @@ const list = new LinkedList();
 list.append(2);
 list.append(10);
 list.append(20);
-// list.delete(20)
 list.prepend(3);
 list.append(10);
 list.prepend(6);
-console.log(list.head.next.next.next)
+list.deleteLastValue();
+list.deleteLastValue();
+list.deleteLastValue();
+list.deleteLastValue();
+list.deleteLastValue();
+console.log(list.head)
+console.log('found node:', list.find(3));
