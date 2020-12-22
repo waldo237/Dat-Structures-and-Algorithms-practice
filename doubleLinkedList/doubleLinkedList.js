8b4172ac5874debab2948144955e651de776d27b
waldo237 <waldo23792@gmail.com>
2020-12-21 23:49:04 -0400

    just finished my practice

--- doubleLinkedList/doubleLinkedList.js
+++ doubleLinkedList/doubleLinkedList.js
@@ -1,8 +1,8 @@
 class Node {
-    constructor(data, previous, next) {
+    constructor(data, next, previous) {
         this.data = data;
-        this.previous = previous;
         this.next = next;
+        this.previous = previous;
     }
 }
 
@@ -11,6 +11,13 @@ class DoubleLinkedList {
         this.head = null;
         this.tail = null;
     }
+    isEqual(a, b) {
+        function compatator(a, b) {
+            if (a === b) return 0;
+            return (a < b) ? -1 : 1;
+        }
+        return compatator === 0;
+    }
 
     prepend(data) {
         const node = new Node(data);
@@ -18,6 +25,7 @@ class DoubleLinkedList {
             this.head.previous = node;
         }
         this.head = node;
+
         if (!this.tail) {
             this.tail = node;
         }
@@ -26,15 +34,136 @@ class DoubleLinkedList {
     }
 
     append(data) {
-      const node = new Node(data, this.head);
-      if(!this.head){
-        this.head = node;
+        const node = new Node(data, this.head);
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+            return this;
+        }
+
+        this.tail.next = node;
+        node.previous = this.tail;
         this.tail = node;
         return this;
     }
 
-    this.tail.next = node;
-    node.previous = this.tail;
-    this.tail = node;
+    delete(data) {
+        if (!this.head) return null;
+
+        let deletedNode = null;
+        let currrentNode = this.head;
+
+        while (currrentNode) {
+            if (this.isEqual(currrentNode.data, data)) {
+                deletedNode = currrentNode;
+                if (deletedNode === this.head) {
+                    this.head = deletedNode.next;
+                    if (this.head) {
+                        this.head.previous = null;
+                    }
+                    if (deletedNode === this.tail) {
+                        this.tail = null;
+                    }
+                } else if (deletedNode === this.tail) {
+                    this.tail = deletedNode.previous;
+                    this.tail.next = null;
+                } else {
+                    const previousNode = deletedNode.previous;
+                    const nextNode = deletedNode.next;
+                    previousNode.next = nextNode;
+                    nextNode.previous = previousNode;
+
+                }
+            }
+            currrentNode = currrentNode.next;
+        }
+        return deletedNode;
+    }
+
+    find(value) {
+        if (!this.head) return null;
+        const { isEqual } = this;
+        let currentNode = this.head;
+        while (currentNode) {
+            if (isEqual(currentNode.data, value)) return currentNode;
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+    deleteFirst(){
+        if(!this.head) return null;
+
+        const deletedNode = this.head;
+
+        if(this.head.next){
+            this.head = this.head.next;
+            this.head.previous = null;
+        }else{
+            this.head = null;
+            this.tail = null;
+        }
+        return deletedNode;
+    }
+    deleteLast(){
+        if(!this.tail) return null;
+
+        const deletedTail = this.tail;
+        if(this.head === this.tail){
+            this.head = null;
+            this.tail = null;
+
+            return deletedTail;
+        }
+        this.tail = this.tail.previous;
+        this.tail.next = null;
+
+    }
+
+    reverse(){
+        let currentNode = this.head;
+        let previousNode = null;
+        let nextNode = null;
+
+
+        while(currentNode){
+            previousNode = currentNode.previous;
+            nextNode = currentNode.next;
+
+            currentNode.next = previousNode;
+            currentNode.previous = nextNode;
+
+            previousNode = currentNode;
+            currentNode = nextNode;
+
+            this.tail = this.head;
+            this.head = previousNode;
+            return this;
+        }
+    }
+
+    toArray(){
+        const nodes = [];
+        let currrentNode = this.head;
+
+        while(currrentNode){
+            nodes.push(currrentNode);
+            currrentNode = currrentNode.next;
+        }
+        return nodes;
     }
 }
+
+const list = new DoubleLinkedList();
+list.append(2);
+list.append(10);
+list.append(20);
+list.prepend(3);
+list.append(20);
+list.append(10);
+list.prepend(6);
+// list.delete(6);
+// list.delete(3);
+list.prepend(3);
+// list.deleteLast();
+
+console.log(list);
