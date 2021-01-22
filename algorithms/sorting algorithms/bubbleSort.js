33c4a367bf85d03bd8e24f7051155224f65deaaf
waldo237 <waldo23792@gmail.com>
2021-01-21 23:31:50 -0400

    done for the day

--- algorithms/sorting algorithms/bubbleSort.js
+++ algorithms/sorting algorithms/bubbleSort.js
@@ -3,7 +3,7 @@ function swap(arr, index1, index2) {
     arr[index1] = arr[index2];
     arr[index2] = temp;
 }
-
+ 
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j <= i; j++) {
