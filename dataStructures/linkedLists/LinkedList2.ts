ba6988093f37c3ad2cfb37fbeac78066c4afe2fe
waldo237 <waldo23792@gmail.com>
2021-01-26 23:38:24 -0400

    added typesscript to another file

--- dataStructures/linkedLists/LinkedList2.ts
+++ dataStructures/linkedLists/LinkedList2.ts
@@ -1,18 +1,33 @@
-"use strict";
-var LinkedListNode2 = /** @class */ (function () {
-    function LinkedListNode2(value, next) {
+
+
+interface ILinkedListNode{
+    value:number |null,
+    next:ILinkedListNode |null
+}
+class LinkedListNode2 implements ILinkedListNode{
+    /**
+     * @param {number} value
+     * @param {Node} next
+     */
+    public value:number;
+    public next:LinkedListNode2
+    constructor(value:number, next:LinkedListNode2) {
         this.value = value;
-        this.next = next || null;
+        this.next = next ||null;
     }
-    return LinkedListNode2;
-}());
-var LinkedList2 = /** @class */ (function () {
-    function LinkedList2() {
+}
+
+class LinkedList2 {
+    private head:LinkedListNode2 |null;
+    private tail:LinkedListNode2 |null;
+    constructor() {
         this.head = null;
         this.tail = null;
     }
-    LinkedList2.prototype.append = function (data) {
-        var node = new LinkedListNode2(data, this.head);
+
+
+    append(data:number): LinkedList2{
+        const node = new LinkedListNode2(data, this.head);
         if (!this.head) {
             this.head = node;
             this.tail = node;
@@ -21,84 +36,92 @@ var LinkedList2 = /** @class */ (function () {
         this.tail.next = node;
         this.tail = node;
         return this;
-    };
-    LinkedList2.prototype.prepend = function (data) {
-        var node = new LinkedListNode2(data, this.head);
+    }
+
+    prepend(data:number) {
+        const node = new LinkedListNode2(data, this.head);
         this.head = node;
         if (!this.tail) {
             this.tail = node;
         }
         return this;
-    };
-    LinkedList2.prototype.delete = function (data) {
+    }
+
+    delete(data) {
         if (!this.head) {
             return null;
         }
-        var deletedNode = null;
-        /*if the data to be deleted is in the head,
+        let deletedNode = null;
+
+        /*if the data to be deleted is in the head, 
         point the head to the next node
         */
         while (this.head && this.equal(this.head.value, data)) {
             deletedNode = this.head;
             this.head = this.head.next;
         }
-        /*if the data to be deleted is in the next node,
+
+        /*if the data to be deleted is in the next node, 
        point the next node to the one next to it.
        */
-        var currentNode = this.head;
+        let currentNode = this.head;
         while (currentNode && currentNode.next) {
             if (this.equal(currentNode.next.value, data)) {
                 deletedNode = currentNode.next;
                 currentNode.next = currentNode.next.next;
-            }
-            else {
+            } else {
                 currentNode = currentNode.next;
             }
         }
+
         if (this.equal(this.tail.value, data)) {
             this.tail = currentNode;
         }
+
         return deletedNode;
-    };
-    LinkedList2.prototype.deleteLastValue = function () {
-        var deletedTail = this.tail;
+    }
+
+    deleteLastValue() {
+        const deletedTail = this.tail;
+
         if (this.head === this.tail) {
             this.head = null;
             this.tail = null;
+
             return deletedTail;
         }
-        var currentNode = this.head;
+
+        let currentNode = this.head;
         while (currentNode.next) {
             if (!currentNode.next.next) {
                 currentNode.next = null;
-            }
-            else {
+            } else {
                 currentNode = currentNode.next;
             }
         }
         this.tail = currentNode;
         return deletedTail;
-    };
-    LinkedList2.prototype.deleteFirstValue = function () {
-        if (!this.head) {
+    }
+
+    deleteFirstValue(){
+        if(!this.head){
             return null;
         }
-        var deletedHead = this.head;
-        if (this.head.next) {
+        const deletedHead = this.head;
+        if(this.head.next){
             this.head = this.head.next;
-        }
-        else {
+        }else{
             this.head = null;
             this.tail = null;
         }
         return deletedHead;
-    };
-    LinkedList2.prototype.find = function (value) {
-        if (value === void 0) { value = undefined; }
+    }
+
+    find(value = undefined) {
         if (!this.head) {
             return null;
         }
-        var currentNode = this.head;
+        let currentNode = this.head;
         while (currentNode) {
             if (value !== undefined && this.equal(currentNode.value, value)) {
                 return currentNode;
@@ -106,10 +129,11 @@ var LinkedList2 = /** @class */ (function () {
             currentNode = currentNode.next;
         }
         return null;
-    };
-    return LinkedList2;
-}());
-var list = new LinkedList();
+    }
+
+}
+
+const list = new LinkedList();
 list.append(2);
 list.append(10);
 list.append(20);
@@ -121,5 +145,5 @@ list.deleteLastValue();
 list.deleteLastValue();
 list.deleteLastValue();
 list.deleteLastValue();
-console.log(list.head);
+console.log(list.head)
 console.log('found node:', list.find(3));
