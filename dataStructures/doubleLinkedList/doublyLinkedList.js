9d81c108bde0d79950d230c76504c3f9078f3985
waldo237 <waldo23792@gmail.com>
2021-02-22 07:47:50 -0400

    practiced doublylinkedlist

--- dataStructures/doubleLinkedList/doublyLinkedList.js
+++ dataStructures/doubleLinkedList/doublyLinkedList.js
@@ -16,72 +16,67 @@ var DoublyLinkedList = /** @class */ (function () {
         this.tail;
     }
     DoublyLinkedList.prototype.append = function (data) {
-        var node = new DoublyLinkedListNode(data, null, null);
+        var newNode = new DoublyLinkedListNode(data, null, null);
         if (!this.tail) {
-            this.head = node;
-            this.tail = node;
+            this.head = newNode;
+            this.tail = newNode;
             this.size++;
         }
         else {
-            this.tail.next = node;
-            node.prev = this.tail;
-            this.tail = node;
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
             this.size++;
         }
     };
     DoublyLinkedList.prototype.prepend = function (data) {
-        var node = new DoublyLinkedListNode(data, this.head, null);
+        var newNode = new DoublyLinkedListNode(data, null, null);
         if (!this.head) {
-            this.head = node;
-            this.tail = node;
-            this.size++;
+            this.head = newNode;
+            this.tail = newNode;
         }
         else {
-            this.head.prev = node;
-            this.head = node;
-            this.size++;
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
         }
     };
     DoublyLinkedList.prototype.remove = function (data) {
-        if (!this.head || !this.tail)
-            return null;
-        var deletedNode = null;
-        if (this.head.data === data) {
+        var _a, _b;
+        var deletedNode = null, currentNode = this.head;
+        // If HEAD is going to be deleted...
+        if (((_a = this.head) === null || _a === void 0 ? void 0 : _a.data) === data) {
+            deletedNode = this.head;
             this.head = this.head.next;
             this.head.prev = null;
             this.size--;
             return deletedNode;
         }
-        var currentNode = this.head;
+        // If tail is going to be deleted...
+        if (((_b = this.tail) === null || _b === void 0 ? void 0 : _b.data) === data) {
+            deletedNode = this.tail;
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+            this.size--;
+            return deletedNode;
+        }
         while (currentNode) {
-            if (currentNode.data === data) {
-                deletedNode = currentNode;
+            if ((currentNode === null || currentNode === void 0 ? void 0 : currentNode.data) === data) {
                 if (!currentNode.next)
-                    this.tail = this.tail.prev; //update the tail
-                if (currentNode.next)
-                    currentNode.next.prev = currentNode.prev;
-                if (currentNode.prev)
-                    currentNode.prev.next = currentNode.next;
+                    this.tail = this.tail.prev;
+                currentNode.next.prev = currentNode.prev;
+                currentNode.prev.next = currentNode.next;
+                deletedNode = currentNode;
                 this.size--;
-                return deletedNode;
             }
             currentNode = currentNode.next;
         }
         return deletedNode;
     };
-    DoublyLinkedList.prototype.removeFirst = function () {
-        if (!this.head)
-            return null;
-        var deletedNode = this.head;
-        this.head = this.head.next;
-        this.head.prev = null;
-        this.size--;
-        return deletedNode;
-    };
     DoublyLinkedList.prototype.reverse = function () {
         if (this.head) {
-            var currentNode = this.head;
-            var temp = null;
+            var currentNode = this.head, temp = null;
+            ;
             while (currentNode) {
                 temp = currentNode.prev;
                 currentNode.prev = currentNode.next;
@@ -101,6 +96,92 @@ var DoublyLinkedList = /** @class */ (function () {
     };
     return DoublyLinkedList;
 }());
+// class DoublyLinkedList<T>{
+//     head: DoublyLinkedListNode<T> | null = null;
+//     tail: DoublyLinkedListNode<T> | null = null;
+//     size: number = 0;
+//     constructor() {
+//         this.head;
+//         this.tail;
+//     }
+//     public append(data: T): void {
+//         const node: DoublyLinkedListNode<T> = new DoublyLinkedListNode<T>(data, null, null);
+//         if (!this.tail) {
+//             this.head = node;
+//             this.tail = node;
+//             this.size++;
+//         } else {
+//             this.tail.next = node;
+//             node.prev = this.tail;
+//             this.tail = node;
+//             this.size++;
+//         }
+//     }
+//     public prepend(data: T): void {
+//         const node = new DoublyLinkedListNode<T>(data, this.head, null);
+//         if (!this.head) {
+//             this.head = node;
+//             this.tail = node;
+//             this.size++;
+//         } else {
+//             this.head.prev = node;
+//             this.head = node;
+//             this.size++;
+//         }
+//     }
+//     public remove(data: T): DoublyLinkedListNode<T> | null {
+//         if (!this.head || !this.tail) return null;
+//         let deletedNode: DoublyLinkedListNode<T> | null = null;
+//         if (this.head.data === data) {
+//             this.head = this.head.next;
+//             this.head!.prev = null;
+//             this.size--;
+//             return deletedNode
+//         }
+//         let currentNode: DoublyLinkedListNode<T> | null = this.head;
+//         while (currentNode) {
+//             if (currentNode.data === data) {
+//                 deletedNode = currentNode;
+//                 if (!currentNode.next) this.tail = this.tail.prev //update the tail
+//                 if (currentNode.next) currentNode.next.prev = currentNode.prev;
+//                 if (currentNode.prev) currentNode.prev.next = currentNode.next;
+//                 this.size--;
+//                 return deletedNode;
+//             }
+//             currentNode = currentNode.next;
+//         }
+//         return deletedNode;
+//     }
+//     public removeFirst(): DoublyLinkedListNode<T> | null {
+//         if (!this.head) return null;
+//         let deletedNode: DoublyLinkedListNode<T> | null = this.head;
+//         this.head = this.head.next;
+//         this.head!.prev = null;
+//         this.size--;
+//         return deletedNode
+//     }
+//     public reverse(): void {
+//         if (this.head) {
+//             let currentNode: DoublyLinkedListNode<T> | null = this.head;
+//             let temp: DoublyLinkedListNode<T> | null = null;
+//             while (currentNode) {
+//                 temp = currentNode.prev;
+//                 currentNode.prev = currentNode.next;
+//                 currentNode.next = temp;
+//                 currentNode = currentNode.prev;
+//             }
+//             this.head = this.tail;
+//         }
+//     }
+//     public print(): void {
+//         let currentNode: DoublyLinkedListNode<T> | null;
+//         currentNode = this.head;
+//         while (currentNode) {
+//             console.log(currentNode.data);
+//             currentNode = currentNode.next;
+//         }
+//     }
+// }
 var list = new DoublyLinkedList();
 console.log('the size is', list.size);
 list.append('uno');
@@ -110,11 +191,12 @@ list.append('tres');
 console.log('the size is', list.size);
 list.append('cuatro');
 list.prepend('zero');
+// list.reverse()
+console.log('the size is', list.size);
+// list.removeFirst();
+// list.removeFirst();
+list.remove('zero');
+console.log('the size is', list.size);
 list.reverse();
-console.log('the size is', list.size);
-list.removeFirst();
-list.removeFirst();
-list.remove('dos');
-console.log('the size is', list.size);
 list.print();
 // console.log(list.tail)
--- dataStructures/doubleLinkedList/doublyLinkedList.ts
+++ dataStructures/doubleLinkedList/doublyLinkedList.ts
@@ -1,7 +1,8 @@
 class DoublyLinkedListNode<T> {
-    data: T;
+    data: T
     next: DoublyLinkedListNode<T> | null;
     prev: DoublyLinkedListNode<T> | null;
+
     constructor(data: T, next: DoublyLinkedListNode<T> | null, prev: DoublyLinkedListNode<T> | null) {
         this.data = data;
         this.next = next;
@@ -10,87 +11,78 @@ class DoublyLinkedListNode<T> {
 }
 
 class DoublyLinkedList<T>{
+
     head: DoublyLinkedListNode<T> | null = null;
     tail: DoublyLinkedListNode<T> | null = null;
     size: number = 0;
     constructor() {
         this.head;
         this.tail;
-
     }
 
     public append(data: T): void {
-        const node: DoublyLinkedListNode<T> = new DoublyLinkedListNode<T>(data, null, null);
+        const newNode = new DoublyLinkedListNode<T>(data, null, null);
         if (!this.tail) {
-            this.head = node;
-            this.tail = node;
+            this.head = newNode;
+            this.tail = newNode;
             this.size++;
         } else {
-            this.tail.next = node;
-            node.prev = this.tail;
-            this.tail = node;
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
             this.size++;
         }
     }
 
     public prepend(data: T): void {
-        const node = new DoublyLinkedListNode<T>(data, this.head, null);
+        const newNode = new DoublyLinkedListNode<T>(data, null, null);
         if (!this.head) {
-            this.head = node;
-            this.tail = node;
-            this.size++;
-
+            this.head = newNode;
+            this.tail = newNode;
         } else {
-            this.head.prev = node;
-            this.head = node;
-            this.size++;
-
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
         }
-
-
     }
 
     public remove(data: T): DoublyLinkedListNode<T> | null {
-        if (!this.head || !this.tail) return null;
+        let deletedNode = null, currentNode = this.head;
 
-        let deletedNode: DoublyLinkedListNode<T> | null = null;
-        if (this.head.data === data) {
+        // If HEAD is going to be deleted...
+        if (this.head?.data === data) {
+            deletedNode = this.head;
             this.head = this.head.next;
             this.head!.prev = null;
             this.size--;
-            return deletedNode
+            return deletedNode;
+        }
+        // If tail is going to be deleted...
+        if (this.tail?.data === data) {
+            deletedNode = this.tail;
+            this.tail = this.tail.prev;
+            this.tail!.next = null;
+            this.size--;
+            return deletedNode;
         }
 
-        let currentNode: DoublyLinkedListNode<T> | null = this.head;
         while (currentNode) {
-            if (currentNode.data === data) {
+            if (currentNode?.data === data) {
+                if (!currentNode.next) this.tail = this.tail!.prev
+                currentNode!.next!.prev = currentNode.prev;
+                currentNode!.prev!.next = currentNode.next;
                 deletedNode = currentNode;
-                if (!currentNode.next) this.tail = this.tail.prev //update the tail
-                if (currentNode.next) currentNode.next.prev = currentNode.prev;
-                if (currentNode.prev) currentNode.prev.next = currentNode.next;
                 this.size--;
-                return deletedNode;
             }
             currentNode = currentNode.next;
         }
         return deletedNode;
     }
 
-    public removeFirst(): DoublyLinkedListNode<T> | null {
-        if (!this.head) return null;
-        let deletedNode: DoublyLinkedListNode<T> | null = this.head;
-
-        this.head = this.head.next;
-        this.head!.prev = null;
-        this.size--;
-        return deletedNode
-
-    }
-
     public reverse(): void {
         if (this.head) {
-            let currentNode: DoublyLinkedListNode<T> | null = this.head;
-            let temp: DoublyLinkedListNode<T> | null = null;
+            let currentNode: DoublyLinkedListNode<T> | null = this.head, 
+            temp: DoublyLinkedListNode<T> | null = null;;
 
             while (currentNode) {
                 temp = currentNode.prev;
@@ -98,11 +90,9 @@ class DoublyLinkedList<T>{
                 currentNode.next = temp;
                 currentNode = currentNode.prev;
             }
-        
             this.head = this.tail;
         }
     }
-
     public print(): void {
         let currentNode: DoublyLinkedListNode<T> | null;
         currentNode = this.head;
@@ -110,27 +100,132 @@ class DoublyLinkedList<T>{
             console.log(currentNode.data);
             currentNode = currentNode.next;
         }
+   
     }
-
 }
 
+// class DoublyLinkedList<T>{
+//     head: DoublyLinkedListNode<T> | null = null;
+//     tail: DoublyLinkedListNode<T> | null = null;
+//     size: number = 0;
+//     constructor() {
+//         this.head;
+//         this.tail;
+
+//     }
+
+//     public append(data: T): void {
+//         const node: DoublyLinkedListNode<T> = new DoublyLinkedListNode<T>(data, null, null);
+//         if (!this.tail) {
+//             this.head = node;
+//             this.tail = node;
+//             this.size++;
+//         } else {
+//             this.tail.next = node;
+//             node.prev = this.tail;
+//             this.tail = node;
+//             this.size++;
+//         }
+//     }
+
+//     public prepend(data: T): void {
+//         const node = new DoublyLinkedListNode<T>(data, this.head, null);
+//         if (!this.head) {
+//             this.head = node;
+//             this.tail = node;
+//             this.size++;
+
+//         } else {
+//             this.head.prev = node;
+//             this.head = node;
+//             this.size++;
+
+//         }
+
+
+//     }
+
+//     public remove(data: T): DoublyLinkedListNode<T> | null {
+//         if (!this.head || !this.tail) return null;
+
+//         let deletedNode: DoublyLinkedListNode<T> | null = null;
+//         if (this.head.data === data) {
+//             this.head = this.head.next;
+//             this.head!.prev = null;
+//             this.size--;
+//             return deletedNode
+//         }
+
+//         let currentNode: DoublyLinkedListNode<T> | null = this.head;
+//         while (currentNode) {
+//             if (currentNode.data === data) {
+//                 deletedNode = currentNode;
+//                 if (!currentNode.next) this.tail = this.tail.prev //update the tail
+//                 if (currentNode.next) currentNode.next.prev = currentNode.prev;
+//                 if (currentNode.prev) currentNode.prev.next = currentNode.next;
+//                 this.size--;
+//                 return deletedNode;
+//             }
+//             currentNode = currentNode.next;
+//         }
+//         return deletedNode;
+//     }
+
+//     public removeFirst(): DoublyLinkedListNode<T> | null {
+//         if (!this.head) return null;
+//         let deletedNode: DoublyLinkedListNode<T> | null = this.head;
+
+//         this.head = this.head.next;
+//         this.head!.prev = null;
+//         this.size--;
+//         return deletedNode
+
+//     }
+
+//     public reverse(): void {
+//         if (this.head) {
+//             let currentNode: DoublyLinkedListNode<T> | null = this.head;
+//             let temp: DoublyLinkedListNode<T> | null = null;
+
+//             while (currentNode) {
+//                 temp = currentNode.prev;
+//                 currentNode.prev = currentNode.next;
+//                 currentNode.next = temp;
+//                 currentNode = currentNode.prev;
+//             }
+
+//             this.head = this.tail;
+//         }
+//     }
+
+//     public print(): void {
+//         let currentNode: DoublyLinkedListNode<T> | null;
+//         currentNode = this.head;
+//         while (currentNode) {
+//             console.log(currentNode.data);
+//             currentNode = currentNode.next;
+//         }
+//     }
+
+// }
+
 const list = new DoublyLinkedList<string>();
-console.log('the size is',list.size)
+console.log('the size is', list.size)
 list.append('uno')
-console.log('the size is',list.size)
+console.log('the size is', list.size)
 list.append('dos')
 list.append('tres')
-console.log('the size is',list.size)
+console.log('the size is', list.size)
 list.append('cuatro')
 list.prepend('zero')
-list.reverse()
-console.log('the size is',list.size)
-list.removeFirst();
-list.removeFirst();
-list.remove('dos')
-console.log('the size is',list.size)
+// list.reverse()
+console.log('the size is', list.size)
+// list.removeFirst();
+// list.removeFirst();
+list.remove('zero')
+console.log('the size is', list.size)
 
-
+list.reverse();
 list.print();
 
 // console.log(list.tail)
