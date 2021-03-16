1cd32336901bc9f599d452e6a2b761d98c8ff4e3
waldo237 <waldo23792@gmail.com>
2021-03-16 06:31:12 -0400

    save merge

--- algorithms/sorting/merge-sort-3-14-21.js
+++ algorithms/sorting/merge-sort-3-14-21.js
@@ -1,21 +1,38 @@
 "use strict";
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spread = (this && this.__spread) || function () {
+    for (var ar = [], i = 0; i < arguments.length; i++) ar = ar.concat(__read(arguments[i]));
+    return ar;
+};
 function merge(left, right) {
     var sorted = [];
     while (left.length && right.length) {
-        if (left[0] < right[0]) {
-            sorted.push(left.shift());
-        }
-        else {
-            sorted.push(right.shift());
-        }
-        ;
+        (left[0] < right[0])
+            ? sorted.push(left.shift())
+            : sorted.push(right.shift());
     }
-    return sorted.concat(left.slice().concat(right.slice()));
+    ;
+    return sorted.concat.apply(sorted, __spread(left, right));
 }
 function mergeSort(array) {
-    if (array.length <= 1)
-        return array;
     var length = array.length;
+    if (length <= 1)
+        return array;
     var middle = Math.floor(length / 2), left = mergeSort(array.slice(0, middle)), right = mergeSort(array.slice(middle, length));
     return merge(left, right);
 }
--- algorithms/sorting/merge-sort-3-14-21.ts
+++ algorithms/sorting/merge-sort-3-14-21.ts
@@ -2,19 +2,17 @@
 function merge<T>(left: T[], right: T[]): Array<T | undefined> {
   let sorted: Array<T | undefined> = [];
   while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      sorted.push(left.shift());
-    } else {
-      sorted.push(right.shift());
-    };
-  }
-  return sorted.concat(left.slice().concat(right.slice()))
+     (left[0] < right[0])
+     ?sorted.push(left.shift())
+     :sorted.push(right.shift());
+  };
+  return  sorted.concat(...left, ...right)
 }
 
  
-function mergeSort<T>(array: Array<T | undefined>): Array<T | undefined> {
-  if (array.length <= 1) return array;
+function mergeSort<T>(array: T[]): Array<T | undefined> {
   const { length } = array;
+  if (length <= 1) return array;
   const middle = Math.floor(length / 2),
     left = mergeSort(array.slice(0, middle)),
     right = mergeSort(array.slice(middle, length));
