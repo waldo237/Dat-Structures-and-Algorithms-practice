844a1b27d4757bf600a38d0337fea2207dbb8ce6
waldo237 <waldo23792@gmail.com>
2021-03-17 06:10:52 -0400

    added the last touch to the quicksort

--- algorithms/sorting/quicksort-p-3-17-21.ts
+++ algorithms/sorting/quicksort-p-3-17-21.ts
@@ -1,30 +1,5 @@
-import { Compare, defaultCompare, ICompareFunction, swap } from '../../util';
 
-function partition(array: any[], left: number, right: number) {
-  const pivot = array[Math.floor((right + left) / 2)];
-  let i = left;
-  let j = right;
-
-  while (i <= j) {
-    while (array[i] < pivot) {
-      i++;
-    }
-
-    while (array[j] > pivot) {
-      j--;
-    }
-
-    if (i <= j) {
-      swap(array, i, j);
-      i++;
-      j--;
-    }
-  }
-
-  return i;
-};
-
-function partitionP<T>(items: T[], left: number, right: number) {
+function partition<T>(items: T[], left: number, right: number) {
   const pivot = items[Math.floor(left + right / 2)];
   let L = left;
   let R = right;
@@ -37,31 +12,35 @@ function partitionP<T>(items: T[], left: number, right: number) {
     while (items[R] > pivot) {
       R--;
     }
-    
+
     if (L <= R) {
       swap(items, L, R);
       L++;
       R--;
     }
   }
+  return L;
 }
 
-function quick<T>(array: T[], left: number, right: number) {
+function swap(array: any[], a: number, b: number) {
+  /* const temp = array[a];
+  array[a] = array[b];
+  array[b] = temp; */
+  [array[a], array[b]] = [array[b], array[a]];
+}
+
+function quick<T>(items: T[], left: number, right: number) {
   let index;
+  if (items.length > 1) {
+    index = partition(items, left, right);
 
-  if (array.length > 1) {
-    index = partition(array, left, right);
+    if (left < index - 1) quick(items, left, index - 1)
 
-    if (left < index - 1) {
-      quick(array, left, index - 1);
-    }
+    if (index < right) quick(items, index, right);
 
-    if (index < right) {
-      quick(array, index, right);
-    }
   }
-  return array;
-};
+  return items;
+}
 
 export function quickSort<T>(array: T[]) {
   return quick(array, 0, array.length - 1);
