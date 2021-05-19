21bb9093f4cb4fe4fda5f757c8bd3c73ff04e231
waldo237 <waldo23792@gmail.com>
2021-05-19 19:13:40 -0400

    practiced algorithms with python

---
+++ algorithms/fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    assert n >= 0 and int(n) == n, "This function cannot be negative and non integer."
+    if n in [0, 1]:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(-7))
