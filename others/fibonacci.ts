52e822192d53977497f9950253b0ec0ed103cec8
waldo237 <waldo23792@gmail.com>
2021-09-23 22:59:54 -0400

    first commit

--- others/fibonacci.ts
+++ others/fibonacci.ts
@@ -1,11 +1,17 @@
 export function fibonacci(n: number): number {
-  if (n < 1) { return 0; } // {1}
-  if (n <= 2) { return 1; } // {2}
+  if (n < 1) {
+    return 0;
+  } // {1}
+  if (n <= 2) {
+    return 1;
+  } // {2}
   return fibonacci(n - 1) + fibonacci(n - 2); // {3}
 }
 
 export function fibonacciIterative(n: number) {
-  if (n < 1) { return 0; }
+  if (n < 1) {
+    return 0;
+  }
   let fibNMinus2 = 0;
   let fibNMinus1 = 1;
   let fibN = n;
@@ -19,10 +25,14 @@ export function fibonacciIterative(n: number) {
 }
 
 export function fibonacciMemoization(n: number) {
-  if (n < 1) { return 0; }
+  if (n < 1) {
+    return 0;
+  }
   const memo = [0, 1];
   const fibonacciMem = (num: number): number => {
-    if (memo[num] != null) { return memo[num]; }
+    if (memo[num] != null) {
+      return memo[num];
+    }
     return (memo[num] = fibonacciMem(num - 1) + fibonacciMem(num - 2));
   };
   return fibonacciMem(n);
